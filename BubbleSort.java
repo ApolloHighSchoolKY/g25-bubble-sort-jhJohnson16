bfb11cd65c93e40fba9d9e15f57062caf3965cb8
jhJohnson16 <jacob.johnson4@stu.daviess.kyschools.us>
2025-04-18 03:57:31 +0000

    Let me Play Crazy cattle 3D plss😭💔

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,12 +1,6 @@
 public class BubbleSort
 {
 
-	public static void main(String[] args)
-  {
-    int[] myNums = {12, 15, 0, 44, 13, 1, 2};
-		int passes;
-		int comparisons;
-		int tempNum;
 
 		//Loop once for each pass, where passes is one less than the number of items.
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
@@ -16,8 +10,20 @@ public class BubbleSort
 			//The next pass will use one less comparison
     
 		//End Loop for passes
+	
+	public static int [] bubbleSorter(int[] nums){
+		int passes  = 0;
+		int tempNum;
 
-
-
+			while(passes <= nums.length -1){
+				for(int i=0; i<nums.length-1; i++){
+					tempNum = nums[i+1];
+					nums[i+1] = nums[i];
+					nums[i] = tempNum;
+				}
+			}
+			passes++;
+	
+	return nums;
   }
 }
